5dada454841290cc2b3c52ba9472e0cfad8776ef
Rohit Nandi <rohitnandibmp@gmail.com>
2023-09-03 02:13:10 +0530

    checkpoint

--- types.ts
+++ types.ts
@@ -2,5 +2,5 @@
 import { Server, Member, Profile, Channel } from "@prisma/client";
 
 export type ServerWithMembersWithProfles = Server & {
-    members: Member & { profile: Profile }[];
+    members: (Member & { profile: Profile })[];
 };
