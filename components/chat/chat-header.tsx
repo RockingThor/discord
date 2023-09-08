c2075e4d7932e14794b181127224fdadaee078ee
Rohit Nandi <rohitnandibmp@gmail.com>
2023-09-08 16:18:31 +0530

    socket is set up

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from "lucide-react";
 import React from "react";
 import MobileToggle from "../mobile-toggle";
 import UserAvatar from "../user-avatar";
+import SocketIndicator from "../socket-indicator";
 
 interface ChatHeaderProps {
     serverId: string;
@@ -15,7 +16,7 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
             <MobileToggle serverId={serverId} />
             {type === "channel" && (
-                <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+                <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
             )}
             {type === "conversation" && (
                 <UserAvatar
@@ -26,6 +27,9 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
             </p>
+            <div className="ml-auto flex items-center">
+                <SocketIndicator />
+            </div>
         </div>
     );
 };
