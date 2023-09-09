31062bae562df236d954fee601c73e7fed3b5db8
Rohit Nandi <rohitnandibmp@gmail.com>
2023-09-09 23:33:34 +0530

    after deleting functionality

--- components/modals/delete-message-modal.tsx
+++ components/modals/delete-message-modal.tsx
@@ -13,15 +13,13 @@ import { useModal } from "@/hooks/use-modal-store";
 import { Button } from "../ui/button";
 
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import qs from "query-string";
 
 const DeleteMessageModal = () => {
     const { isOpen, onClose, type, data } = useModal();
-    const router = useRouter();
 
     const isModalOpen = isOpen && type == "deleteMessage";
-    const { server, channel } = data;
+    const { apiUrl, query } = data;
 
     const [loading, setLoading] = useState(false);
 
@@ -29,15 +27,11 @@ const DeleteMessageModal = () => {
         try {
             setLoading(true);
             const url = qs.stringifyUrl({
-                url: `/api/channels/${channel?.id}`,
-                query: {
-                    serverId: server?.id,
-                },
+                url: apiUrl || "",
+                query,
             });
             await axios.delete(url);
             onClose();
-            router.refresh();
-            router.push(`/servers/${server?.id}`);
         } catch (error) {
             console.log(error);
         } finally {
@@ -53,15 +47,10 @@ const DeleteMessageModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Delete Channel
+                        Delete Message
                     </DialogTitle>
                     <DialogDescription className=" text-center text-zinc-500">
-                        Are you sure you want to delete{" "}
-                        <span className="font-semibold text-indigo-500">
-                            #{channel?.name}
-                        </span>
-                        ?
-                        <br />
+                        Are you sure you want to delete? <br />
                         <span className="text-[#e93a3a]">
                             This action can&apos;t be reversed!
                         </span>
