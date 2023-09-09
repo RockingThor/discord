15d490daec005fa56b0429cb698d52a8f2e6d6b6
Rohit Nandi <rohitnandibmp@gmail.com>
2023-09-10 02:03:23 +0530

    downgraded to next 13.4.12 and added config so that socket behaves perfectly. Web socket is now bwhaving perfectly

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -18,6 +18,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useModal } from "@/hooks/use-modal-store";
+import { useParams, useRouter } from "next/navigation";
 
 interface ChatItemProps {
     id: string;
@@ -108,16 +109,32 @@ const ChatItem = ({
         });
     }, [content, form]);
 
+    const params = useParams();
+    const router = useRouter();
+
+    const onMemberClick = () => {
+        if (member.id === currentMember.id) {
+            return;
+        }
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+    };
+
     return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
-                <div className="cursor-pointer hover:drop-shadow-md transition">
+                <div
+                    className="cursor-pointer hover:drop-shadow-md transition"
+                    onClick={onMemberClick}
+                >
                     <UserAvatar src={member.profile.imageUrl} />
                 </div>
                 <div className="flex flex-col w-full">
                     <div className="flex items-center gap-x-2">
                         <div className="flex items-center">
-                            <p className="font-semibold text-sm hover:underline cursor-pointer">
+                            <p
+                                className="font-semibold text-sm hover:underline cursor-pointer"
+                                onClick={onMemberClick}
+                            >
                                 {member.profile.name}
                             </p>
                             <ActionTooltip label={member.role}>
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -6,6 +6,7 @@ import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import ChatItem from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -39,8 +40,12 @@ const ChatMessages = ({
     type,
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`;
     const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
         useChatQuery({ queryKey, apiUrl, paramKey, paramValue });
+
+    useChatSocket({ queryKey, addKey, updateKey });
 
     if (status === "loading") {
         return (
